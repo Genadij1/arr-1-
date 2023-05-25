b200e8799a29abe64999f99824cfbb7a790a4171
Геннадий <@DESKTOP-6NN51IE>
2023-05-25 19:35:21 +0300

    1

---
+++ массив/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<cstdlib>
+#include<climits>
+#include<ctime>
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+	const int a = 20;
+	int arr[a + 1],end,begin;
+	int n, max_num = 0;
+	cout << "Enter end num: ";
+	cin >>  end;
+	for (int i = 0; i < a; i++) {
+		arr[i] = rand()%end;
+		n = arr[i];
+		cout << n << " ";
+		if (n > max_num) {
+			max_num = n;
+		}
+	}
+	cout <<endl<< "max num = " << max_num << endl;
+
+	system("pause>nul");
+	return 0;
+}
