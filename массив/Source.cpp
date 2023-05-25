6b343ec65acdd7adbb17622f4d04ab62bfb439bb
Геннадий <@DESKTOP-6NN51IE>
2023-05-25 19:39:51 +0300

    3

--- массив/Source.cpp
+++ массив/Source.cpp
@@ -8,7 +8,7 @@ int main() {
 	srand(time(NULL));
 	const int a = 20;
 	int arr[a + 1],end,begin;
-	int n, max_num = 0,min_num= numeric_limits<int>::max();
+	int n, max_num = 0, min_num = numeric_limits<int>::max();
 	cout << "Enter end num: ";
 	cin >>  end;
 	for (int i = 0; i < a; i++) {
@@ -18,8 +18,12 @@ int main() {
 		if (n > max_num) {
 			max_num = n;
 		}
+		if (n < min_num) {
+			min_num = n;
+		}
 	}
 	cout <<endl<< "max num = " << max_num << endl;
+	cout << endl << "min num = " << min_num << endl;
 
 	system("pause>nul");
 	return 0;
