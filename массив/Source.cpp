c6a88d29ea2c6524cd7baa1154a8e9dafbd60875
Геннадий <@DESKTOP-6NN51IE>
2023-05-25 19:53:04 +0300

    1

--- массив/Source.cpp
+++ массив/Source.cpp
@@ -5,7 +5,18 @@
 using namespace std;
 
 int main() {
-	
+
+    int a, max_a = 0, min_a = numeric_limits<int>::max();
+    const int b = 12;
+    int arr[b + 1];
+    arr[0] = 1;
+    for (int k = 0; k <= b; k++) {
+        arr[k] = k;
+    }
+    for (int i = 1; i <= b; i++) {
+        cout << "Enter profit for " << arr[i] << " month: ";
+        cin >> a;
+    }
 
 	system("pause>nul");
 	return 0;
